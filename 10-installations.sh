aff447091c52042d2057432f3b76370182d3637d
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-22 23:51:30 +0530

    installation-with-exit-status

--- 10-installations.sh
+++ 10-installations.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if[ $USERID -ne 0 ]
+
+if [ $USERID -ne 0 ]
 then
     echo "please run this script with root"
     exit 1 #this command is to exit manually as error came here
