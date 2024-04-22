12f3e125241e854bf5fff41b58a4f34dc6aa408a
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-22 18:07:13 +0530

    conditions-if-else

--- 09-conditions-if.sh
+++ 09-conditions-if.sh
@@ -1,10 +1,12 @@
-#!bin/bash
+#!/bin/bash
 
-NUMBER=$!
+NUMBER=$1
 
-if [ $NUMBER -gt  10 ]
+if [ $NUMBER -gt 10 ]
 then
-   echo "given number $NUMBER is greter than 10"
+    echo "Given number $NUMBER is greater than 10"
 else
-   echo "given number $NUMBER is less than 10"
+    echo "Given number $NUMBER is less than 10" 
 fi
+
+#-gt, -lt, -eq, -ge, -le
