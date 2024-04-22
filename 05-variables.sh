ac4213799bfd19ccd6a4798500b7d86f528f2c88
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-22 15:56:07 +0530

    read-command-usage-for confidential

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please enter the username:"
+read USERNAME
+echo "please enter the password"
+read PASSWORD
+
+echo "show the username entered $USERNAME and password entered $PASSWORD"
