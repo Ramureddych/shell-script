a764d1d8a645e8f37eb1416c176366535dadc0c2
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-22 15:59:06 +0530

    read-command-with-s-usage-for confidential

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "please enter the username:"
-read -s  USERNAME
+read -s  USERNAME  #if you keep -s it will not shows the value which we enters and also here USERNAME is variable
 echo "please enter the password"
-read -s PASSWORD
+read -s PASSWORD  
 
 echo "show the username entered $USERNAME and password entered $PASSWORD"
