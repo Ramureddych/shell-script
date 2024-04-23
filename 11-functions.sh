45bfa3e22890ace69fa6e5f24b64a765b843dab3
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-23 15:27:26 +0530

    functions-usage

---
+++ 11-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is failing"
+    else
+        echo "$2 is succcess"
+    fi    
+}
+
+
+
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run with root"
+    exit 1
+else 
+    echo "you are super user"
+fi
+
+dnf install mysql -y 
+validate $? "installing mysql"
+
+dnf insall git -y 
+validate $? "installing git"
+
