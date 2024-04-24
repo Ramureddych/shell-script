a33edf28be0c0fb9f3c33d44f2531528e7fee348
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-24 11:02:42 +0530

    installing-packages-through-loops

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+echo "$LOGFILE"
 RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
