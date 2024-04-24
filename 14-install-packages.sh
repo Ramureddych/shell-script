e6c1caf5e01191406d2052057188937b4bfeebb4
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-24 10:58:50 +0530

    installing-packages-through-loops

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,18 +4,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NOCOLOUR="\e[0m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo -e "$2...$R FAILURE $N"
+        echo -e "$2...$RED FAILURE $NOCOLOUR"
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "$2...$GREEN SUCCESS $NOCOLOUR"
     fi
 }
 
@@ -33,7 +33,7 @@ do
     dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
     then
-        echo -e "$i already installed...$Y SKIPPING $N"
+        echo -e "$i already installed...$YELLOW SKIPPING $NOCOLOUR"
     else
         dnf install $i -y &>>$LOGFILE
         VALIDATE $? "Installation of $i"
